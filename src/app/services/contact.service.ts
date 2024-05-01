04088c8b0dd16479a122abbecbf3a5b857bdfce0
paslavskyi9roman <paslavsky.roman@gmail.com>
2024-05-01 11:51:44 +0300

    implementing fetching contact from a localstorage via service

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import {ContactModel} from "../models/contact.model";
+
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
   constructor() { }
+
+  getContacts(): ContactModel[] {
+    const localStorageContacts = localStorage.getItem('contacts');
+    let contacts: ContactModel[] = [];
+
+    if (localStorageContacts) {
+      contacts = JSON.parse(localStorageContacts) as ContactModel[];
+    }
+
+    return contacts;
+  }
 }
