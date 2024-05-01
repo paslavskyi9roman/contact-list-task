2e1fd9acc671a88ffaa07509f01c6698941372e1
paslavskyi9roman <paslavsky.roman@gmail.com>
2024-05-01 15:11:31 +0300

    adding validation to edit component

--- src/app/components/edit-contact/edit-contact.component.ts
+++ src/app/components/edit-contact/edit-contact.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { ContactModel } from '../../models/contact.model';
 import { ContactService } from '../../services/contact.service';
+import { ContactModel } from '../../models/contact.model';
 
 @Component({
   selector: 'app-edit-contact',
@@ -10,36 +11,55 @@ import { ContactService } from '../../services/contact.service';
   styleUrls: ['./edit-contact.component.scss'],
 })
 export class EditContactComponent implements OnInit {
-  contact: ContactModel | undefined = {
-    id: '',
-    name: '',
-    surname: '',
-    phone: '',
-    email: '',
-    address: '',
-    dateOfBirth: new Date(),
-  };
+  contactForm: FormGroup = new FormGroup({});
 
   constructor(
     private contactService: ContactService,
     private router: Router,
-    private route: ActivatedRoute
-  ) {}
-
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {
+    this.createForm();
+  }
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.contact = this.contactService.getContactById(id);
-    } else {
-      this.router.navigate(['/contacts']);
+    if (!id) {
+      this.navigateToContacts();
+      return;
     }
+
+    const contact = this.contactService.getContactById(id);
+    if (!contact) {
+      this.navigateToContacts();
+      return;
+    }
+
+    this.contactForm.patchValue(contact);
+  }
+
+  private navigateToContacts(): void {
+    this.router.navigate(['/contacts']);
+  }
+
+  createForm(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      dateOfBirth: [new Date(), Validators.required],
+    });
   }
 
   editContact(): void {
-    if (!this.contact) {
-      return;
+    if (this.contactForm.valid) {
+      const updatedContact: ContactModel = {
+        ...this.contactForm.value,
+        id: this.route.snapshot.paramMap.get('id') || '',
+      };
+      this.contactService.updateContact(updatedContact);
+      this.router.navigate(['/contacts', updatedContact.id]);
     }
-    this.contactService.updateContact(this.contact);
-    this.router.navigate(['/contacts', this.contact.id]);
   }
 }
