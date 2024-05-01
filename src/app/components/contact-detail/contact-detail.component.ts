e2a2248eb4680c84ec659aee7ec2656d5615987f
paslavskyi9roman <paslavsky.roman@gmail.com>
2024-05-01 13:11:39 +0300

    adding some styles to the app

--- src/app/components/contact-detail/contact-detail.component.ts
+++ src/app/components/contact-detail/contact-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ContactModel } from '../../models/contact.model';
 import { ContactService } from '../../services/contact.service';
@@ -7,20 +7,26 @@ import { ContactService } from '../../services/contact.service';
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
-  styleUrls: ['./contact-detail.component.scss']
+  styleUrls: ['./contact-detail.component.scss'],
 })
 export class ContactDetailComponent implements OnInit {
   contact: ContactModel | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private contactService: ContactService
-  ) {}
+    private contactService: ContactService,
+    private router: Router,
+  ) {
+  }
 
   ngOnInit(): void {
     const id: string | null = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.contact = this.contactService.getContactById(id);
     }
+  }
+
+  goBack(): void {
+    this.router.navigate(['/contacts']);
   }
 }
