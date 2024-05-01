a36f09935d110ff79bf9e5e8a9e6ca7effbceac3
paslavskyi9roman <paslavsky.roman@gmail.com>
2024-05-01 13:25:35 +0300

    adding address property

--- src/app/mock-data/initial-data.mock.ts
+++ src/app/mock-data/initial-data.mock.ts
@@ -7,7 +7,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Doe',
     phone: '1234567890',
     dateOfBirth: new Date(1990, 1, 1),
-    email: 'john.doe@example.com'
+    email: 'john.doe@example.com',
+
+    address: "123 Main St, New York, USA"
   },
   {
     id: '2',
@@ -15,7 +17,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Doe',
     phone: '0987654321',
     dateOfBirth: new Date(1992, 2, 2),
-    email: 'jane.doe@example.com'
+    email: 'jane.doe@example.com',
+
+    address: "323 Main St, New York, USA"
   },
   {
     id: '3',
@@ -23,7 +27,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Smith',
     phone: '1122334455',
     dateOfBirth: new Date(1985, 5, 5),
-    email: 'bob.smith@example.com'
+    email: 'bob.smith@example.com',
+
+    address: "321 Main St, New York, USA"
   },
   {
     id: '4',
@@ -31,7 +37,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Johnson',
     phone: '1223344556',
     dateOfBirth: new Date(1980, 4, 4),
-    email: 'alice.johnson@example.com'
+    email: 'alice.johnson@example.com',
+
+    address: "123 Main St, New York, USA"
   },
   {
     id: '5',
@@ -39,7 +47,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Brown',
     phone: '1334455667',
     dateOfBirth: new Date(1983, 3, 3),
-    email: 'charlie.brown@example.com'
+    email: 'charlie.brown@example.com',
+
+    address: "412 Main St, New York, USA"
   },
   {
     id: '6',
@@ -47,7 +57,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Williams',
     phone: '1445566778',
     dateOfBirth: new Date(1982, 2, 2),
-    email: 'david.williams@example.com'
+    email: 'david.williams@example.com',
+
+    address: "'456 Oak St, New York, USA"
   },
   {
     id: '7',
@@ -55,7 +67,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Davis',
     phone: '1556677889',
     dateOfBirth: new Date(1981, 1, 1),
-    email: 'eve.davis@example.com'
+    email: 'eve.davis@example.com',
+
+    address: "865 Main St, New York, USA"
   },
   {
     id: '8',
@@ -63,7 +77,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Miller',
     phone: '1667788990',
     dateOfBirth: new Date(1980, 12, 12),
-    email: 'frank.miller@example.com'
+    email: 'frank.miller@example.com',
+
+    address: "'626 Oak St, New York, USA"
   },
   {
     id: '9',
@@ -71,7 +87,9 @@ export const contactMock: ContactModel[] = [
     surname: 'Wilson',
     phone: '1778899001',
     dateOfBirth: new Date(1979, 11, 11),
-    email: 'grace.wilson@example.com'
+    email: 'grace.wilson@example.com',
+
+    address: "637 Main St, New York, USA"
   },
   {
     id: '10',
@@ -79,6 +97,8 @@ export const contactMock: ContactModel[] = [
     surname: 'Moore',
     phone: '1889900112',
     dateOfBirth: new Date(1978, 10, 10),
-    email: 'harry.moore@example.com'
+    email: 'harry.moore@example.com',
+
+    address: "'156 Oak St, New York, USA"
   },
 ];
--- src/app/models/contact.model.ts
+++ src/app/models/contact.model.ts
@@ -5,4 +5,5 @@ export interface ContactModel {
   phone: string;
   dateOfBirth: Date;
   email: string;
+  address: string;
 }
