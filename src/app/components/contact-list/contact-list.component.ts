a2e2961bffecdc8bbbd5b18114297e2e7da29bb1
paslavskyi9roman <paslavsky.roman@gmail.com>
2024-05-01 11:53:19 +0300

    connecting component with the service

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+
+import {ContactModel} from "../../models/contact.model";
+import {ContactService} from "../../services/contact.service";
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.scss'
+  styleUrls: ['./contact-list.component.css']
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
+  contacts: ContactModel[] = [];
+  private contactService: ContactService = Inject(ContactService)
+
+  ngOnInit() {
+    this.getContacts();
+  }
+
+  getContacts() {
+    this.contacts = this.contactService.getContacts();
+  }
 
 }
